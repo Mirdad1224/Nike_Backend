5ace96b086eae5ee03c9e9efc1caa97983b40c67
Mirdad1224 <ali.mirdad@yahoo.com>
2022-12-22 18:38:33 +0330

    create server by express

---
+++ server.js
@@ -0,0 +1,53 @@
+require('dotenv').config()
+require('express-async-errors')
+const express = require('express')
+const path = require('path')
+const errorHandler = require('./middlewares/errorHandler')
+const credentials = require('./middlewares/credentials');
+const cookieParser = require('cookie-parser')
+const cors = require('cors')
+const corsOptions = require('./config/corsOptions')
+const connectDB = require('./config/connectDB')
+const mongoose = require('mongoose')
+const helmet = require('helmet');
+
+const app = express()
+const PORT = process.env.PORT || 8000
+
+connectDB()
+
+// Handle options credentials check - before CORS!
+// and fetch cookies credentials requirement
+app.use(credentials);
+
+app.use(cors(corsOptions))
+
+app.use(express.json())
+
+app.use(helmet())
+app.use(helmet.crossOriginResourcePolicy({policy: 'cross-origin'}))
+
+app.use(cookieParser())
+
+app.use('/', express.static(path.join(__dirname, 'public')))
+
+//* Routes
+app.use('/auth', require('./routes/authRoutes'))
+app.use('/', require('./routes/shopRoutes'))
+app.use('/favorate', require('./routes/favorateRoutes'))
+
+
+app.all('*', (req, res) => {
+    res.json({ message: '404 Not Found' })
+})
+
+app.use(errorHandler)
+
+mongoose.connection.once('open', () => {
+    console.log('Connected to MongoDB')
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+})
+
+mongoose.connection.on('error', err => {
+    console.log(err)
+})
