c7adc8ca308682553ea92d25f92ac456e9cc10f4
Mirdad1224 <ali.mirdad@yahoo.com>
2022-12-22 18:32:35 +0330

    add utility function for connect to db

---
+++ config/connectDB.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  try {
+    mongoose.set("strictQuery", true);
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+module.exports = connectDB;
