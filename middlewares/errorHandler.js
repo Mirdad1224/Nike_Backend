d29bfdce4bf73f725021061214b22ef33c95ef03
Mirdad1224 <ali.mirdad@yahoo.com>
2022-12-22 18:37:51 +0330

    add error handler middleware

---
+++ middlewares/errorHandler.js
@@ -0,0 +1,7 @@
+const errorHandler = (err, req, res, next) => {
+  const status = res.statusCode ? res.statusCode : 500; // server error
+
+  res.status(status).json({ message: err.message, isError: true });
+};
+
+module.exports = errorHandler;
