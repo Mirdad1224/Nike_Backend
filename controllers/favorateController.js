13bb229ef12dfbe0bfbf53a247eb93460a923300
Mirdad1224 <ali.mirdad@yahoo.com>
2022-12-24 18:28:01 +0330

    create favorate controller

---
+++ controllers/favorateController.js
@@ -0,0 +1,48 @@
+const User = require("../models/User");
+
+exports.getFavorates = async (req, res, next) => {
+  const user = req.user;
+  const userInfo = await User.findOne({ username: user })
+    .populate({
+      path: "favorates",
+    })
+    .exec();
+
+  if (!userInfo) {
+    return res.status(401).json({ message: "user not found" });
+  }
+
+  const favorateList = userInfo.favorates;
+
+  return res.status(200).json({ data: favorateList });
+};
+
+exports.postFavorate = async (req, res, next) => {
+  const user = req.user;
+  const { productId } = req.params;
+  const userInfo = await User.findOne({ username: user });
+
+  if (!userInfo) {
+    return res.status(401).json({ message: "user not found" });
+  }
+  const favorateIndex = userInfo.favorates.findIndex((f) => {
+    return f.toString() === productId.toString();
+  });
+
+  let updatedFavorates;
+
+  if (favorateIndex >= 0) {
+    updatedFavorates = userInfo.favorates.filter((f) => {
+      return f.toString() !== productId.toString();
+    });
+    console.log(updatedFavorates);
+    userInfo.favorates = updatedFavorates;
+  } else {
+    updatedFavorates = [...userInfo.favorates, productId];
+    userInfo.favorates = updatedFavorates;
+  }
+
+  await userInfo.save();
+
+  return res.status(200).json({ message: "operation done successfully" });
+};
